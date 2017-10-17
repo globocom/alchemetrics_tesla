9370732e821a25280f3e820be2cda056468d0b0a
Hugo Roque <hugolnx@gmail.com>
2017-10-17 12:59:54 -0200

    Executing middleware before adapter by default

--- lib/tesla/middleware/alchemetrics.ex
+++ lib/tesla/middleware/alchemetrics.ex
@@ -1,14 +1,22 @@
 defmodule Tesla.Middleware.Alchemetrics do
-  def call(%Tesla.Env{url: url, method: method, __module__: module, opts: opts} = env, next, _options) do
+  def call(%Tesla.Env{url: url, method: method, __module__: module, opts: opts} = env, next, [keep_order: true]) do
     route_name = opts[:metric_route_name]
     service_name = service_name_for(module)
     AlchemetricsTesla.report_service_route(method, URI.parse(url), service_name, route_name, fn ->
       Tesla.run(env, next)
     end)
   end
+  def call(env, next, nil) do
+    next = next |> add_before_adapter({__MODULE__, :call, [[keep_order: true]]})
+    Tesla.run(env, next)
+  end
 
   defp service_name_for(module) do
     [[_, service_name]] = Regex.scan(~r{^Elixir\.(.+)$}, to_string(module))
     service_name
+  end
+
+  defp add_before_adapter(next, step) do
+    List.insert_at(next, -2, step)
   end
 end
--- test/support/tesla_client.ex
+++ test/support/tesla_client.ex
@@ -9,3 +9,27 @@ defmodule Support.TeslaClient do
     end
   end)
 end
+
+defmodule Support.TeslaClientWithBaseUrl do
+  use Tesla
+  plug Tesla.Middleware.BaseUrl, "http://mypets.com:8080/users"
+  plug Tesla.Middleware.Alchemetrics
+
+  adapter(fn env ->
+    cond do
+      String.match?(env.url, ~r{pets}) -> %{env | status: 200, body: "pets"}
+    end
+  end)
+end
+
+defmodule Support.TeslaClientWithBaseUrlAfter do
+  use Tesla
+  plug Tesla.Middleware.Alchemetrics
+  plug Tesla.Middleware.BaseUrl, "http://mypets.com:8080/users"
+
+  adapter(fn env ->
+    cond do
+      String.match?(env.url, ~r{pets}) -> %{env | status: 200, body: "pets"}
+    end
+  end)
+end
