70a83580b3873757236c14d17a71f601cba96073
Hugo Roque <hugolnx@gmail.com>
2018-08-28 15:10:40 -0300

    Release 2.0.0

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule AlchemetricsTesla.Mixfile do
   def project do
     [
       app: :alchemetrics_tesla,
-      version: "1.1.4",
+      version: "2.0.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
